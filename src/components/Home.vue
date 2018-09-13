7ce5b2caa1a6135b367568036f8b4cd49af70e61
Kunikazu <kunikazutayama@gmail.com>
2018-09-13 18:10:41 -0400

    first

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="home">
+    <button @click="googleLogin">Googleアカウントでログイン</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'Welcome to MyMarkdown'
+    }
+  },
+  methods: {
+    googleLogin: function() {
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.home {
+    margin-top: 60px;
+}
+</style>
